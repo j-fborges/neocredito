764896131b29b99ed0ea5e550ffa68b091829563
j-fborges <contato.joao.pfb@gmail.com>
2026-05-15 16:52:02 -0300

    style: lint and format ProposalList.tsx

--- src/components/eSignProposal/ProposalList.tsx
+++ src/components/eSignProposal/ProposalList.tsx
@@ -16,7 +16,9 @@ export default function ProposalList({ itens, onRowClick }: ProposalListProps) {
       <thead>
         <tr className="bg-gray-200">
           <th className="p-1 sm:p-2 text-left">{translations.headerId}</th>
-          <th className="p-1 sm:p-2 text-left">{translations.headerCustomer}</th>
+          <th className="p-1 sm:p-2 text-left">
+            {translations.headerCustomer}
+          </th>
           <th className="p-1 sm:p-2 sm:pl-14 text-left">
             {translations.headerStatus}
           </th>
